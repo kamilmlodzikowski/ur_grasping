6109e9a12d0cb652714101ee25500bba3fccf2df
k.mlodzikowski@gmail.com <k.mlodzikowski@gmail.com>
2019-10-02 23:03:47 +0200

    changing to work with robot4.0 sim

--- src/ggcnn_kinova_grasping/ggcnn_kinova_grasping/scripts/run_ggcnn.py
+++ src/ggcnn_kinova_grasping/ggcnn_kinova_grasping/scripts/run_ggcnn.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import time
-import robot_controller #DODANE
 import numpy as np
 import tensorflow as tf
 from keras.models import load_model
@@ -18,14 +17,15 @@ from cv_bridge import CvBridge
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import Image, CameraInfo
 from std_msgs.msg import Float32MultiArray
+from nav_msgs.msg import Path
+from nav_msgs.msg import Odometry
+from tf.transformations import quaternion_from_euler
+from geometry_msgs.msg import PoseStamped
 
 bridge = CvBridge()
 mypath = os.getcwd()
 
 
-#DODANE
-manipulator = robot_controller.Ur3("150.254.47.150", 30003, 30002)
-print manipulator.get_pose()
 
 
 
@@ -43,7 +43,7 @@ img_pub = rospy.Publisher('ggcnn/img/img', Image, queue_size=1)
 grasp_plain_pub = rospy.Publisher('ggcnn/img/grasp_plain', Image, queue_size=1)
 depth_pub = rospy.Publisher('ggcnn/img/depth', Image, queue_size=1)
 ang_pub = rospy.Publisher('ggcnn/img/ang', Image, queue_size=1)
-#cmd_pub = rospy.Publisher('ggcnn/out/command', Float32MultiArray, queue_size=1)
+cmd_pub = rospy.Publisher('/trajectory_planned', Path, queue_size=1)
 
 # Initialise some globals.
 prev_mp = np.array([150, 150])
@@ -53,12 +53,12 @@ ROBOT_Z = 0
 graph = tf.get_default_graph()
 
 # Get the camera parameters
-camera_info_msg = rospy.wait_for_message('/camera/depth/camera_info', CameraInfo)
-K = camera_info_msg.K
-fx = K[0]
-cx = K[2]
-fy = K[4]
-cy = K[5]
+#camera_info_msg = rospy.wait_for_message('/realsense_wrist/depth/camera_info', CameraInfo)
+#K = camera_info_msg.K
+fx = 1 #K[0]
+cx = 0 #K[2]
+fy = 1 #K[4]
+cy = 0 #K[5]
 
 
 # Execution Timing
@@ -112,6 +112,7 @@ def depth_callback(depth_message):
         depth_crop = depth_crop.copy()
         depth_nan = np.isnan(depth_crop).copy()
         depth_crop[depth_nan] = 0
+	print ('crop')
 
     with TimeIt('Inpaint'):
         # open cv inpainting does weird things at the border.
@@ -127,12 +128,14 @@ def depth_callback(depth_message):
         # Back to original size and value range.
         depth_crop = depth_crop[1:-1, 1:-1]
         depth_crop = depth_crop * depth_scale
+	print ('inpaint')
 
     with TimeIt('Calculate Depth'):
         # Figure out roughly the depth in mm of the part between the grippers for collision avoidance.
         depth_center = depth_crop[100:141, 130:171].flatten()
         depth_center.sort()
         depth_center = depth_center[:10].mean() * 1000.0
+	print ('calc_depth')
 
     with TimeIt('Inference'):
         # Run it through the network.
@@ -142,6 +145,7 @@ def depth_callback(depth_message):
 
         points_out = pred_out[0].squeeze()
         points_out[depth_nan] = 0
+	print ('inference')
 
     with TimeIt('Trig'):
         # Calculate the angle map.
@@ -150,15 +154,18 @@ def depth_callback(depth_message):
         ang_out = np.arctan2(sin_out, cos_out)/2.0
 
         width_out = pred_out[3].squeeze() * 150.0  # Scaled 0-150:0-1
+	print ('trig')
 
     with TimeIt('Filter'):
         # Filter the outputs.
         points_out = ndimage.filters.gaussian_filter(points_out, 5.0)  # 3.0
         ang_out = ndimage.filters.gaussian_filter(ang_out, 2.0)
+	print ('filter')
 
     with TimeIt('Control'):
         # Calculate the best pose from the camera intrinsics.
         maxes = None
+	print ('control_0')
 
         ALWAYS_MAX = False  # Use ALWAYS_MAX = True for the open-loop solution.
 
@@ -166,18 +173,22 @@ def depth_callback(depth_message):
             # Track the global max.
             max_pixel = np.array(np.unravel_index(np.argmax(points_out), points_out.shape))
             prev_mp = max_pixel.astype(np.int)
+	    print ('control_if')
         else:
             # Calculate a set of local maxes.  Choose the one that is closes to the previous one.
             maxes = peak_local_max(points_out, min_distance=10, threshold_abs=0.1, num_peaks=3)
+	    print ('control_else')
             if maxes.shape[0] == 0:
-                return
+		print ('control_ret')
+                #return
             max_pixel = maxes[np.argmin(np.linalg.norm(maxes - prev_mp, axis=1))]
-
+	    print ('control_max')
             # Keep a global copy for next iteration.
             prev_mp = (max_pixel * 0.25 + prev_mp * 0.75).astype(np.int)
 
         ang = ang_out[max_pixel[0], max_pixel[1]]
         width = width_out[max_pixel[0], max_pixel[1]]
+	print ('control_A')
 
         # Convert max_pixel back to uncropped/resized image coordinates in order to do the camera transform.
         max_pixel = ((np.array(max_pixel) / 300.0 * crop_size) + np.array([(480 - crop_size)//2, (640 - crop_size) // 2]))
@@ -189,6 +200,7 @@ def depth_callback(depth_message):
         x = (max_pixel[1] - cx)/(fx) * point_depth
         y = (max_pixel[0] - cy)/(fy) * point_depth
         z = point_depth
+	print ('control_B')
 
         if np.isnan(z):
             return
@@ -206,7 +218,7 @@ def depth_callback(depth_message):
         grasp_img[rr, cc, 2] = 0
 
 
-	img_img = img_crop
+        img_img = img_crop
         #img_img[:,:,2] = (points_out * 255.0)
 
         img_img_plain = grasp_img.copy()
@@ -215,6 +227,7 @@ def depth_callback(depth_message):
         img_img[rr+30, cc-11, 0] = 0
         img_img[rr+30, cc-11, 1] = 255
         img_img[rr+30, cc-11, 2] = 0
+	print ('draw')
 
     with TimeIt('Publish'):
         # Publish the output images (not used for control, only visualisation)
@@ -224,6 +237,7 @@ def depth_callback(depth_message):
         img_img.header = depth_message.header
         grasp_pub.publish(grasp_img)
         img_pub.publish(img_img)
+	print ('pub_wysoko')
 
         grasp_img_plain = bridge.cv2_to_imgmsg(grasp_img_plain, 'bgr8')
         grasp_img_plain.header = depth_message.header
@@ -233,26 +247,32 @@ def depth_callback(depth_message):
 
         ang_pub.publish(bridge.cv2_to_imgmsg(ang_out))
 
+	path = Path()
+	pose = PoseStamped()
+	pose.pose.position.x = x/200.0
+	pose.pose.position.y = y/200.0
+	pose.pose.position.z = z/200.0
+	#(qx, qy, qz, qw) = quaternion_from_euler (0, 0, ang)
+	pose.pose.orientation.x = 0
+	pose.pose.orientation.y = 0
+	pose.pose.orientation.z = 0
+	pose.pose.orientation.w = 1
+	print ('pub_nisko')
+	path.poses.append(pose)
+	cmd_pub.publish(path)
+
+
         # Output the best grasp pose relative to camera.
         #cmd_msg = Float32MultiArray()
         #cmd_msg.data = [x, y, z, ang, width, depth_center]
         #cmd_pub.publish(cmd_msg)
-        
-        #DODANE
-        trajectory = list()
-        pose = [x, y, z, ang, width, depth_center]
-        trajectory.append(pose)
-        manipulator.move(trajectory, False, a=0.1, v=0.5)
 
 
-depth_sub = rospy.Subscriber('/camera/depth/image', Image, depth_callback, queue_size=1)
-img_sub = rospy.Subscriber('/camera/rgb/image_color', Image, img_callback, queue_size=1)
+depth_sub = rospy.Subscriber('/realsense_wrist/depth/image_rect_raw', Image, depth_callback, queue_size=1)
+img_sub = rospy.Subscriber('/realsense_wrist/color/image_raw', Image, img_callback, queue_size=1)
+print ('sub')
 #robot_pos_sub = rospy.Subscriber('/m1n6s200_driver/out/tool_pose', PoseStamped, robot_pos_callback, queue_size=1)
 
-#DODANE
-global ROBOT_Z
-pose1 =  manipulator.get_pose()
-ROBOT_Z = pose1[2]
 
 while not rospy.is_shutdown():
     rospy.spin()
