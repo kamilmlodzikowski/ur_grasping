69d86c9cd31b8e782b0956541dc78e6317d631d5
Justyna <j.ataman@tlen.pl>
2019-11-27 09:53:23 +0100

    .data

--- src/ggcnn_kinova_grasping/ggcnn_kinova_grasping/scripts/move.py
+++ src/ggcnn_kinova_grasping/ggcnn_kinova_grasping/scripts/move.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env python
 
 import tools
 import rospy
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Float32MultiArray
+rospy.init_node('move')
 
 
 def convert_pose():
@@ -10,12 +13,12 @@ def convert_pose():
     return pose
 
 
-def move():
-    tools.move(pose_goal, rot_z=0, a=0.08, v=0.5, ip="192.168.1.211", port_write=30003, port_read=30002, check_joits_TF=True)
+def move(pose_g):
+    tools.move(pose_g.data, rot_z=0, a=0.08, v=0.5, ip="192.168.1.211", port_write=30003, port_read=30002, check_joits_TF=True)
 
 
 Pose = rospy.Publisher('/UR5_pose', PoseStamped, queue_size=1)
-pose_goal = rospy.Subscriber('/ggcnn/out/command', move, queue_size=1)
+pose_goal = rospy.Subscriber('/ggcnn/out/command', Float32MultiArray, move, queue_size=1)
 
 while not rospy.is_shutdown():
     rospy.spin()
