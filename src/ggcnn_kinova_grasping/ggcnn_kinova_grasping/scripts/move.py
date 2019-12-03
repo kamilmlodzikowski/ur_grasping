e51bfacc09673087f9144a12f043277b486922be
Justyna <j.ataman@tlen.pl>
2019-12-03 10:23:49 +0100

    working on grip

--- src/ggcnn_kinova_grasping/ggcnn_kinova_grasping/scripts/move.py
+++ src/ggcnn_kinova_grasping/ggcnn_kinova_grasping/scripts/move.py
@@ -2,16 +2,19 @@
 
 import tools
 import rospy
-import robot_controller
-import time
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float32MultiArray
+import time
+
 rospy.init_node('move')
 manipulator = robot_controller.Ur3("192.168.1.211", 30003, 30002)
 time.sleep(5)
 
-
 last_z = 1000
+
+suma = 0
+i = 0
+
 
 def convert_pose():
     pose = PoseStamped()
@@ -20,17 +23,16 @@ def convert_pose():
 
 
 def move(pose_g):
-    print pose_g.data
     global last_z
     if not (pose_g.data[0] == 0 or pose_g.data[1] == 0 or pose_g.data[2] == 0.15 or last_z - pose_g.data[2] < 0.05):
-        tools.move(pose_g, manipulator, rot_z=0, a=0.01, v=0.05, ip="192.168.1.211", port_write=30003, port_read=30002, check_joits_TF=True)
+        tools.move2(pose_g, manipulator, rot_z=0, a=0.01, v=0.05, ip="192.168.1.211", port_write=30003, port_read=30002, check_joits_TF=True)
         last_z = pose_g.data[2]
 
         # print pose_g.data
         # tools.get_pose(manipulator)
     # else:
     #     msg = rospy.wait_for_message('/ggcnn/out/command', Float32MultiArray)
-    #     move(msg)global last_z
+    #     move(msg)
 
 Pose = rospy.Publisher('/UR5_pose', PoseStamped, queue_size=1)
 pose_goal = rospy.Subscriber('/ggcnn/out/command', Float32MultiArray, move, queue_size=1)
